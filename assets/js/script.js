4c9d4510903f6529cecf09f441ae99cd2ffb7fd0
stev232 <stev232@yahoo.com>
2022-12-18 15:42:26 -0600

    got base functionality in the javascript and added a method to locally save top 10 high scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,75 @@
-/*localStorage.getItem("highScores");
-localStorage.setItem("highScores", arrScore);*/
-var scoreBoard = localStorage.getItem("highScores");
+var scoreBoard = JSON.parse(localStorage.getItem("highScores"));
+if(scoreBoard==null) {
+    scoreBoard = [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0];
+}
+var isComplete = false;
+var timer = 60;
+var score = 0;
 
-var arrQuestions = ["What is  the statement to set a border radius in css?", ];
-var arrAnswers = [["border-radius", "borderRadius", "border_radius", "border.radius"], ];
+const timeEl = document.getElementById("timer");
+const liEl = ["firstPlace", "secondPlace", "thirdPlace", "fourthPlace","fifthPlace",
+                "sixthPlace", "seventhPlace", "eighthPlace", "ninethPlace", "tenthPlace"];
 
-var startQuiz = document.getElementById("startBtn");
-var question = document.getElementById("question");
-var answerOne = document.getElementById("answerOne");
-var answerTwo = document.getElementById("answerTwo");
-var answerThree = document.getElementById("answerThree");
-var answerFour = document.getElementById("answerFour");
+const arrQuestions = ["What is  the statement to set a border radius in css?", "What command do you use to link a style sheet?" ];
+const arrAnswers = [["border-radius", "borderRadius", "border_radius", "border.radius"], 
+        ['<link rel="stylesheet" href="./assets/css/style.css" />', '<a href="./assets/css/style.css" rel="stylesheet"> ', '<stylesheet path="./assets/css/style.css" />', '<link rel="./assets/css/style.css" href="stylesheet" />'], ];
+const startQuiz = document.getElementById("startQuiz");
+const question = document.getElementById("questionContainer");
+const answerOne = document.getElementById("answerOne");
+const answerTwo = document.getElementById("answerTwo");
+const answerThree = document.getElementById("answerThree");
+const answerFour = document.getElementById("answerFour");
 
-startQuiz.addEventListener("click", function() {
-    var score = startTime();
-    if(scoreBoard==null) {
-        scoreBoard = [0,0,0,0,0,0,0,0,0,0];
+function generateScoreBoard() {
+    if(scoreBoard != null && scoreBoard[0] != 0){
+        for(var i = 0; i < scoreBoard.length; i++) {
+            if(scoreBoard[i] == 0) {
+                break;
+            } else {
+                document.getElementById(liEl[i]).textContent  = scoreBoard[i];
+            }
+        }
     }
+}
+
+function saveScore() {
     for(var i = 0; i < scoreBoard.length; i++) {
         if(score > scoreBoard[i]) {
-            for(var x = 10; x > scoreBoard.length - i; x--) {
+            for(var x = 9; x > i; x--) {
                 scoreBoard[x] = scoreBoard[x-1];
             }
             scoreBoard[i] = score;
-            exit;
+            break;
         }
     }
-    localStorage.setItem("highScores", scoreBoard);
+    localStorage.setItem("highScores", JSON.stringify(scoreBoard));
+    generateScoreBoard();
+}
+
+startQuiz.addEventListener("click", function() {
+    isComplete = false;
+    timer = 60;
+
+    question.style.display="block";
+    answerOne.style.display="block";
+    answerTwo.style.display="block";
+    answerThree.style.display="block";
+    answerFour.style.display="block";
+    startQuiz.style.display="none";
+
+    startTime();
 });
 
 function startTime() {
-    var timer = 60;
+
+    question.textContent = arrQuestions[0];
+    answerOne.textContent = arrAnswers[0][0];
+    answerTwo.textContent = arrAnswers[0][1];
+    answerThree.textContent = arrAnswers[0][2];
+    answerFour.textContent = arrAnswers[0][3];
 
     answerOne.addEventListener("click", function() {
-
+        isComplete = true;
     });
 
     answerTwo.addEventListener("click", function() {
@@ -53,9 +89,19 @@ function startTime() {
         timeEl.textContent = timer;
   
         if(isComplete) {
-            return timer;
+            score = timer;
+            question.style.display="none";
+            answerOne.style.display="none";
+            answerTwo.style.display="none";
+            answerThree.style.display="none";
+            answerFour.style.display="none";
+            startQuiz.style.display="block";
+            saveScore();
+            clearInterval(timerInterval);
         } else if(timer === 0) {
-            return 0;
+            clearInterval(timerInterval);
         }
     }, 1000);
-  }
+}
+
+generateScoreBoard();
